6f9cbb34a16f0b4a53d2ac98b7e69028cfdf9958
IrenaDemchenkova <iren.demchenkova@gmail.com>
2022-10-26 15:44:59 +0300

    Функциональные интерфейсы вынесены в отдельные классы

--- src/Worker.java
+++ src/Worker.java
@@ -1,21 +1,14 @@
 public class Worker {
-    @FunctionalInterface
-    public interface OnTaskDoneListener{
-       void onDone (String result);
-    }
-    @FunctionalInterface
-    public interface OnTaskErrorListener {
-        void onError(String error);
-    }
-    private OnTaskDoneListener callback;
-    private OnTaskErrorListener errorCallback;
-    public Worker (OnTaskDoneListener callback, OnTaskErrorListener errorCallback){
+    private final OnTaskDoneListener callback;
+    private final OnTaskErrorListener errorCallback;
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
         this.callback = callback;
         this.errorCallback = errorCallback;
     }
 
-        public void start() {
-        for (int i=0; i<100; i++) {
+    public void start() {
+        for (int i = 0; i < 100; i++) {
             if (i == 33) {
                 errorCallback.onError("Task " + i + " is failed!");
             } else {
@@ -23,5 +16,4 @@ public class Worker {
             }
         }
     }
-
 }
